b3fe6f582a9ad7608d53b94478b28d493790ba29
yunusozturk@msn.com <yunusozturk@msn.com>
2023-01-25 22:21:53 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './Styles/index.css';
+import '../node_modules/'
 import App from './App'; 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'; 
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  <App />
-</BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
  
